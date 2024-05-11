5cc66b3eee71eadfbb8ff7f7984f67fd439b5f70
heehunjung <heehunjung@DESKTOP-8BE2SL1>
2024-05-11 13:05:20 +0900

    수정 날짜 비교 구현

--- main.c
+++ main.c
@@ -29,17 +29,17 @@ int main(){
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    
+
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    
+
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
@@ -49,20 +49,36 @@ void filetime2(){
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+    printf("date compare\n");
+
+    if (time1->tm_year > time2->tm_year) {
+        printf("text1 is later fixed(data)\n");
+    } else if (time1->tm_year < time2->tm_year) {
+        printf("text2 is later fixed(data)\n");
+    } else if (time1->tm_mon > time2->tm_mon) {
+        printf("text1 is later fixed(data)\n");
+    } else if (time1->tm_mon < time2->tm_mon) {
+        printf("text2 is later fixed(data)\n");
+    } else if (time1->tm_mday > time2->tm_mday) {
+        printf("text1 is later fixed(data)\n");
+    } else if (time1->tm_mday < time2->tm_mday) {
+        printf("text2 is later fixed(data)\n");
+    } else {
+        printf("same date\n");
+    }
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+
 }
